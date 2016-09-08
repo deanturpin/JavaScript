63e69e6021afb11994b5431f5fc794c735f04c01
deanturpin <git@lvbtn.uk>
2016-09-08 18:00:03 +0100

    extract time

--- Node.js/readline.js
+++ Node.js/readline.js
@@ -3,7 +3,13 @@ var readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin });
 
 // Read one line at a time
-rl.on('line', (input) => {
+rl.on('line', (line) => {
 
-	console.log(input)
+	// Extract time
+	match = /[^ ]+/.exec(line)
+
+	if (match)
+		console.log(match[0])
+	else
+		console.log("no")
 })
