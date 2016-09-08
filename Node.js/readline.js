bdd4b4d0059ed08e155fa5e55923a3c94c44e940
deanturpin <git@lvbtn.uk>
2016-09-08 20:50:20 +0100

    simplify

--- Node.js/readline.js
+++ Node.js/readline.js
@@ -1,3 +1,6 @@
+// Usage
+// ip r | node readline.js
+
 var readline = require('readline');
 
 const rl = readline.createInterface({ input: process.stdin });
@@ -5,14 +8,5 @@ const rl = readline.createInterface({ input: process.stdin });
 // Read one line at a time
 rl.on('line', (line) => {
 
-	// Extract time
-	// time = /[^ ]+/.exec(line)
-	// if (time)
-		// console.log(time.pop())
-
-	// Extract end points
-	endPoints = /([^ ]+) > ([^ ]+):/.exec(line)
-
-	if (endPoints)
-		console.log(endPoints[1] + "\t" + endPoints[2])
+	console.log(line)
 })
