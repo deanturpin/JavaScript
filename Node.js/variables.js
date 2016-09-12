a1c579d9983a2b1d58721942555bdce58a6de0be
deanturpin <git@lvbtn.uk>
2016-09-12 08:01:48 +0100

    let and const

--- Node.js/variables.js
+++ Node.js/variables.js
@@ -1,10 +1,20 @@
+"use strict";
+
+let i = 10;
+
 (function () {
 
-	// var i;
+	let i;
 	for (i = 0; i < 10; ++i) {
 		console.log("before", i);
 	}
 
 })();
+
+let x = {
+	y: 5
+}
+
+const blah = 6;
 
 console.log("after", i);
