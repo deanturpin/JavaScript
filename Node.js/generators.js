32be84f0045c167a4769af329a538f444ace9a88
deanturpin <git@lvbtn.uk>
2016-09-17 12:19:05 +0100

    missing .

--- Node.js/generators.js
+++ Node.js/generators.js
@@ -2,5 +2,5 @@ const fetch = require("node-fetch");
 
 fetch("http://jsonplaceholder.typicode.com/posts/1")
 	.then(response => response.json())
-	then(post => post.title)
+	.then(post => post.title)
 	.then(x => console.log("Title", x));
