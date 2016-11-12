38251bac73ca31d1f97d2be18197ca8b3227f251
deanturpin <git@lvbtn.uk>
2016-11-12 18:45:48 +0000

    use repeat rather than for loop

--- Node.js/recursion.js
+++ Node.js/recursion.js
@@ -4,12 +4,7 @@ const iterations = 3
 
 function something(i, side) {
 
-	// Create indent
-	var indent = ""
-	for (var d = 0; d < iterations - i; ++d)
-		indent += " "
-		
-	console.log(indent, i, side)
+	console.log(" ".repeat(iterations - i), i, side)
 
 	// Recurse - make sure to declare iterator as var
 	if (i > 0)
