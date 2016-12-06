a522e13a660ddef927b032592a05a5a5f79d60ae
deanturpin <git@lvbtn.uk>
2016-12-06 19:04:53 +0000

    first version

---
+++ Node.js/prime.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+"use strict"
+
+var iterations = 0
+
+function prime(i) {
+
+	console.log("Calculate prime numbers up to " + i)
+
+	// A prime number has no positive divisors apart from 1 and itself
+}
+
+prime(10)
