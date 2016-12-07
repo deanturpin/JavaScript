f32670bfea5e3236b5cd9481b85539b26fb96840
deanturpin <git@lvbtn.uk>
2016-12-07 17:25:53 +0000

    start to simplify

--- webgl/webgl.js
+++ webgl/webgl.js
@@ -5,10 +5,11 @@ function createShader(gl, type, source) {
 	var shader = gl.createShader(type)
 	gl.shaderSource(shader, source)
 	gl.compileShader(shader)
+
 	var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
-	if (success) {
+
+	if (success)
 		return shader
-	}
 
 	console.log(gl.getShaderInfoLog(shader))
 	gl.deleteShader(shader)
@@ -21,10 +22,11 @@ function createProgram(gl, vertexShader, fragmentShader) {
 	gl.attachShader(program, vertexShader)
 	gl.attachShader(program, fragmentShader)
 	gl.linkProgram(program)
+
 	var success = gl.getProgramParameter(program, gl.LINK_STATUS)
-	if (success) {
+
+	if (success)
 		return program
-	}
 
 	console.log(gl.getProgramInfoLog(program))
 	gl.deleteProgram(program)
@@ -34,30 +36,14 @@ onload = function() {
 
 	var canvas = document.getElementById("canvas")
 
-	// https://en.wikipedia.org/wiki/WebGL
-
-	/*
-	WebGL runs on the GPU on your computer. As such you need to provide the code
-	that runs on that GPU. You provide that code in the form of pairs of
-	functions. Those 2 functions are called a vertex shader and a fragment
-	shader and they are each written in a very strictly typed C/C++ like
-	language called GLSL. (GL Shader Language). Paired together they are called
-	a program.
-	*/
-
 	// Create WebGL rendering context
 	var gl = canvas.getContext("webgl")
 
-	// INITIALISE
-	console.log("INITIALISE")
-
 	// Vertex shader
-	console.log("vertex shader")
 	var vertexShaderSource = document.getElementById("2d-vertex-shader").text
 	var fragmentShaderSource = document.getElementById("2d-fragment-shader").text
 
 	// Fragment shader
-	console.log("fragment shader")
 	var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
 	var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
 
@@ -66,19 +52,18 @@ onload = function() {
 	var positionBuffer = gl.createBuffer()
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
-	var positions = [
-		0, 0,
-		0, 0.5,
-		0.7, 0,
-	]
+	// Array of points
+	var positions = []
+	for (var i = 0; i < 1; i += .1) {
+
+		positions[positions.length] = i
+		positions[positions.length] = i/2
+	}
 
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
-	// RENDER
-	console.log("RENDER")
-
 	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-	gl.clearColor(0.0, 1.0, 1.0, 1.0)
+	// gl.clearColor(0.0, 0, 1.0, 1.0)
 	gl.enable(gl.DEPTH_TEST)
 
 	gl.useProgram(program)
@@ -88,17 +73,40 @@ onload = function() {
 	// Bind the position buffer.
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 	 
-	// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-	var size = 2;          // 2 components per iteration
-	var type = gl.FLOAT;   // the data is 32bit floats
-	var normalize = false; // don't normalize the data
-	var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-	var offset = 0;        // start at the beginning of the buffer
+	gl.vertexAttribPointer(
+		positionAttributeLocation,
+		2,							// Components per iteration
+		gl.FLOAT,					// 32-bit floats
+		false,						// Don't normalise
+		0,							// Stride
+		0							// Offset
+	)
 
-	gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
+	/*
 
-	var primitiveType = gl.TRIANGLES;
-	var offset = 0;
-	var count = 3;
-	gl.drawArrays(primitiveType, offset, count);
+	This is the GPU code. You'll notice it isn't quite JavaScript. Most
+	tutorials tend to put it between HTML script tags but then it's not
+	immediately obvious how it relates to the rest of the code.
+
+	Note: you can't drop the terminating semi-colon.
+
+	var vertCode = 'attribute vec3 coordinates;' +
+
+	   'void main(void) {' +
+	         ' gl_Position = vec4(coordinates, 1.0);' +
+			       'gl_PointSize = 10.0;'+
+				      '}';
+	*/
+
+	// GL primitives
+
+	// POINTS
+	// LINES
+	// LINE_LOOP
+	// LINE_STRIP
+	// TRIANGLES
+	// TRIANGLE_STRIP
+	// TRIANGLE_FAN
+
+	gl.drawArrays(gl.POINTS, 0, positions.length/2);
 }
